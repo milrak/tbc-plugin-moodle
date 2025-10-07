fa78837623ba3edea405b95693d1fdd81919903b
Milrak Pereira Pessoa <milrak.pessoa@fametro.edu.br>
2025-10-07 13:55:03 -0400

    Ajustando nova versão

--- rm/version.php
+++ rm/version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025092701;
+$plugin->version   = 2025100701;
 $plugin->requires  = 2022112800;    // Moodle 4.1.0+
 $plugin->component = 'auth_rm'; 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.0';
+$plugin->release   = '2.0.0';

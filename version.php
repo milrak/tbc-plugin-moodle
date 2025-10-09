343e5cc7d3868516cdeaab59ea341773bf044343
Milrak Pereira Pessoa <milrak.pessoa@fametro.edu.br>
2025-10-09 14:39:20 -0400

    Exibindo mensagem de erro do curl

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025100701;
+$plugin->version   = 2025100901;
 $plugin->requires  = 2022112800;    // Moodle 4.1.0+
 $plugin->component = 'auth_rm'; 
 $plugin->maturity  = MATURITY_STABLE;
